5ab5a77cca965719888f64362e0fa5ab3117e015
Maksim Chemerisuk <chemerisuk@gmail.com>
2020-03-14 12:07:43 +0300

    update ios implementation

--- src/ios/FirebaseCrashPlugin.m
+++ src/ios/FirebaseCrashPlugin.m
@@ -1,20 +1,24 @@
 #import "FirebaseCrashPlugin.h"
-#import <Fabric/Fabric.h>
-#import <Crashlytics/Crashlytics.h>
+
+@import Firebase;
 
 @implementation FirebaseCrashPlugin
 
 - (void)pluginInitialize {
-    NSLog(@"Starting Firebase Crash plugin");
+    NSLog(@"Starting Firebase Crashlytics plugin");
+
+    if(![FIRApp defaultApp]) {
+        [FIRApp configure];
+    }
 }
 
 - (void)log:(CDVInvokedUrlCommand *)command {
-    [self.commandDelegate runInBackground:^{
-        NSString* errorMessage = [command.arguments objectAtIndex:0];
-        CLSNSLog(@"%@", errorMessage);
-        CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
+    NSString* errorMessage = [command.arguments objectAtIndex:0];
+
+    [[FIRCrashlytics crashlytics] log:errorMessage];
+
+    CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)logError:(CDVInvokedUrlCommand*)command {
@@ -23,9 +27,10 @@
     NSNumber *defaultCode = [NSNumber numberWithInt:-1];
     int code = [[command argumentAtIndex:1 withDefault:defaultCode] intValue];
     NSString *domain = [[NSBundle mainBundle] bundleIdentifier];
-    NSError *error = [NSError errorWithDomain: domain code: code userInfo: userInfo];
+    NSError *error = [NSError errorWithDomain:domain code:code userInfo:userInfo];
 
-    [[Crashlytics sharedInstance] recordError:error];
+    [[FIRCrashlytics crashlytics] recordError:error];
+
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
@@ -33,7 +38,8 @@
 - (void)setUserId:(CDVInvokedUrlCommand *)command {
     NSString* userId = [command.arguments objectAtIndex:0];
 
-    [CrashlyticsKit setUserIdentifier:userId];
+    [[FIRCrashlytics crashlytics] setUserID:userId];
+
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
